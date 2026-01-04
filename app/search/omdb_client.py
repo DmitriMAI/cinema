d8a5917377651c1880d39f12d09537cab42cb659
Денис Устинов <lantum@MacBook-Air-lantum.local>
2026-01-04 22:32:04 +0300

    Поправлено логирование. Удален obsolete код из docker-compose.yaml

--- app/search/omdb_client.py
+++ app/search/omdb_client.py
@@ -9,7 +9,7 @@ OMDB_API_KEY = os.getenv("OMDB_API_KEY")
 
 def get_movie_poster(name: str):
     if not OMDB_API_KEY:
-        logger.error("OMDB_API_KEY переменная не установления, возвращаем пустой адрес постера фильма")
+        logger.error("OMDB_API_KEY переменная не установлена, возвращаем пустой адрес постера фильма")
         return ""
 
     logger.info("Запрос адреса постера фильма из OMDb", extra={"movie_name": name})
